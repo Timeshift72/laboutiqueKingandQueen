0ed99f2516b8644568d9b5e566dc664db6d78123
Timeshift72 <dossantosfilipe7@gmail.com>
2022-09-28 16:31:18 +0200

    Application e-commerce exemple

---
+++ src/Controller/OrderController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Controller;
+
+use Stripe\Stripe;
+use App\Classe\Cart;
+use App\Entity\Order;
+use App\Form\OrderType;
+use App\Entity\OrderDetails;
+use Stripe\Checkout\Session;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class OrderController extends AbstractController
+{
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/commande", name="order")
+     */
+    public function index(Cart $cart, Request $request)
+    {
+        if (!$this->getUser()->getAddresses()->getValues())
+        {
+            return $this->redirectToRoute('account_address_add');
+        }
+
+        $form = $this->createForm(OrderType::class, null, [
+            'user' => $this->getUser()
+        ]);
+
+        return $this->render('order/index.html.twig', [
+            'form' => $form->createView(),
+            'cart' => $cart->getFull()
+        ]);
+    }
+
+    /**
+     * @Route("/commande/recapitulatif", name="order_recap", methods={"POST"})
+     */
+    public function add(Cart $cart, Request $request)
+    {
+        $form = $this->createForm(OrderType::class, null, [
+            'user' => $this->getUser()
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $date = new \DateTime();
+            $carriers = $form->get('carriers')->getData();
+
+            $delivery = $form->get('addresses')->getData();
+            $delivery_content = $delivery->getFirstname().' '.$delivery->getLastname();
+            $delivery_content .= '<br/>'.$delivery->getPhone();
+
+            if ($delivery->getCompany()) {
+                $delivery_content .= '<br/>'.$delivery->getCompany();
+            }
+
+            $delivery_content .= '<br/>'.$delivery->getAddress();
+            $delivery_content .= '<br/>'.$delivery->getPostal().' '.$delivery->getCity();
+            $delivery_content .= '<br/>'.$delivery->getCountry();
+
+            // Enregistrer ma commande Order()
+            $order = new Order();
+            $reference = $date->format('dmY').'-'.uniqid();
+            $order->setReference($reference);
+            $order->setUser($this->getUser());
+            $order->setCreatedAt($date);
+            $order->setCarrierName($carriers->getName());
+            $order->setCarrierPrice($carriers->getPrice());
+            $order->setDelivery($delivery_content);
+            $order->setState(0);
+
+            $this->entityManager->persist($order);
+
+            // Enregistrer mes produits OrderDetails()
+            foreach ($cart->getFull() as $product) {
+                $orderDetails = new OrderDetails();
+                $orderDetails->setMyOrder($order);
+                $orderDetails->setProduct($product['product']->getName());
+                $orderDetails->setQuantity($product['quantity']);
+                $orderDetails->setPrice($product['product']->getPrice());
+                $orderDetails->setTotal($product['product']->getPrice() * $product['quantity']);
+                $this->entityManager->persist($orderDetails);
+            }
+
+             $this->entityManager->flush();
+
+            // Stripe::setApiKey('sk_test_51JoXUWCKfaza0VI3i8IF7MQTnH6asFsbQKZ7KR97pHmsU4A83N1IphDU8fP5fsg25msOXOfQOTufpGAYlPGmnyQi003v0DxNES');
+
+            // $YOUR_DOMAIN = 'http://127.0.0.1:8000';
+
+            
+            // $checkout_session = Session::create([
+            //     'line_items' => [[
+            //     # TODO: replace this with the `price` of the product you want to sell
+            //     'price' => '{{PRICE_ID}}',
+            //     'quantity' => 1,
+            // ]],
+            //     'payment_method_types' => [
+            //     'card',
+            // ],
+            //     'mode' => 'payment',
+            //     'success_url' => $YOUR_DOMAIN . '/success.html',
+            //     'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+            // ]);
+
+            // dump($checkout_session->id);
+            // dd($checkout_session);
+
+            return $this->render('order/add.html.twig', [
+                'cart' => $cart->getFull(),
+                'carrier' => $carriers,
+                'delivery' => $delivery_content
+            ]);
+        }
+
+        return $this->redirectToRoute('cart');
+    }
+ }
